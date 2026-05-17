26ba7ea2a14d80599f1f13316ef47024a5a93e30
Krushna <hardekrushna88@gmail.com>
2026-05-18 00:09:21 +0530

    bfs - dfs modify

--- AI/bfs-dfs.cpp
+++ AI/bfs-dfs.cpp
@@ -1,33 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> graph[100];
-bool visited[100];
+unordered_map<int, vector<int>> graph;
+unordered_map<int, bool> visited;
 
 // DFS
-void dfs(int node)
-{
+void dfs(int node){
+
     visited[node] = true;
-
-    cout << node << " ";
-
-    for(int adj : graph[node])
-    {
-        if(!visited[adj])
-            dfs(adj);
-    }
-}
-
-
-queue<int> q;
-
-void bfs(){
-
-    if(q.empty())
-        return;
-
-    int node = q.front();
-    q.pop();
 
     cout << node << " ";
 
@@ -35,80 +15,84 @@ void bfs(){
 
         if(!visited[adj]){
 
-            visited[adj] = true;
-
-            q.push(adj);
+            dfs(adj);
         }
     }
-
-    bfs();
 }
 
-
 // BFS
-void bfs(int start)
-{
-    bool visit[100] = {false};
+void bfs(int start){
+
+    unordered_map<int, bool> visit;
 
     queue<int> q;
 
     q.push(start);
+
     visit[start] = true;
 
-    while(!q.empty())
-    {
+    while(!q.empty()){
+
         int node = q.front();
         q.pop();
 
         cout << node << " ";
 
-        for(int adj : graph[node])
-        {
-            if(!visit[adj])
-            {
+        for(int adj : graph[node]){
+
+            if(!visit[adj]){
+
                 visit[adj] = true;
+
                 q.push(adj);
             }
         }
     }
 }
 
-int main()
-{
-    int n, e;
+int main(){
+
+    /*
+    Sample Input:
+    4 3
+    10 20
+    10 30
+    20 40
+    10
+    */
+
+    int e, n;
 
     cout << "Enter number of nodes and edges: ";
+
     cin >> n >> e;
 
     cout << "Enter edges:\n";
 
-    for(int i=0; i<e; i++)
-    {
+    for(int i=0; i<e; i++){
+
         int u,v;
+
         cin >> u >> v;
 
         graph[u].push_back(v);
+
         graph[v].push_back(u);
     }
 
     int start;
 
     cout << "Enter starting node: ";
+
     cin >> start;
 
     cout << "\nDFS Traversal:\n";
+
     dfs(start);
 
     cout << "\n\nBFS Traversal:\n";
+
     bfs(start);
 
     return 0;
-}
-
-
-
-// try->
-vector<bool> vis(n, false);
-void dfs(int node){
-    vis
 }
